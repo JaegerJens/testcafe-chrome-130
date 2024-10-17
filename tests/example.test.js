1128f60ee50edfb0c1c28cd8a3ae45ceb4353ceb
Jens Jäger <jens.jaeger@mobilex.eu>
2024-10-17 08:02:10 +0200

    create example test

---
+++ tests/example.test.js
@@ -0,0 +1,6 @@
+fixture`Test structure`
+    .page`https://devexpress.github.io/testcafe/example`;
+
+test('My test', async t => {
+    // Starts at http://devexpress.github.io/testcafe/blog/
+}).page`https://devexpress.github.io/testcafe/blog/`;
